24f0a16c0ebbf4156c882f815b6af3296666b3b8
ey_av <eyal@veloxpro.com>
2017-12-08 17:30:16 +0200

    fixing routing issue after notes added

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ import HomePage from './pages/HomePage.js'
 import PlacesApp from './pages/PlacesApp.js'
 import EmailApp from './pages/EmailApp.js'
 import NotesApp from './pages/NotesApp.js'
+import NoteView from './pages/NoteView.js'
 
 const routes = [
     {
